fb9c8be450338b5abc1ae66b4a880384b59a005c
Riccardo Pierini <riccardo.pierini@studenti.unicam.it>
2021-02-24 15:30:04 +0100

    Rifinitura

--- src/main/java/it/unicam/Team151/C3/C3Application.java
+++ src/main/java/it/unicam/Team151/C3/C3Application.java
@@ -1,8 +1,4 @@
 package it.unicam.Team151.C3;
-
-import it.unicam.Team151.C3.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
